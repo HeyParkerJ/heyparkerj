df446bae1a7f6f62a0e449ec1046cf9cf3785704
Parker Johnson <mrstormthebeach@gmail.com>
2019-02-17 09:09:30 -0800

    stylize and implement nav functionality

--- src/components/header.js
+++ src/components/header.js
@@ -1,18 +1,38 @@
 import React from "react"
+import { Link } from "gatsby"
 import styles from './header.module.css'
 
-const NavElement = ({title}) =>
-      <li className={styles.navElement}>
-        {title}
+const NavElement = props =>
+      <li className={styles.list}>
+        <Link to={props.to}
+              className={styles.link}
+              activeClassName={styles.activeLink}
+        >
+          {props.children}
+        </Link>
       </li>
+
+const PartialNavElement = props =>
+  <li className={styles.list}>
+    <Link to={props.to}
+          className={styles.link}
+          activeClassName={styles.activeLink}
+          getProps={({ isPartiallyCurrent }) =>
+                    isPartiallyCurrent
+                    ? { className: styles.activeLink }
+                    : { className: styles.link } }
+    >
+      {props.children}
+    </Link>
+  </li>
 
 export default () => (
   <nav className={styles.header}>
     <ul className={styles.nav}>
-      <NavElement title="Home" />
-      <NavElement title="Blog" />
-      <NavElement title="Projects" />
-      <NavElement title="About" />
+      <NavElement to="/">Home</NavElement>
+      <PartialNavElement to="/blog">Blog</PartialNavElement>
+      <NavElement to="/projects">Projects</NavElement>
+      <NavElement to="/about">About</NavElement>
     </ul>
   </nav>
 )
---
+++ src/pages/blog.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from '../components/layout'
+
+import styles from './index.module.css'
+
+export default () => (
+  <Layout>
+    <h1 className={styles.header}>Blog</h1>
+  </Layout>
+)
