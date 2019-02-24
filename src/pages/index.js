a3e1679efaf2c45efccb3fe02ef49c7eb9e37366
Parker Johnson <mrstormthebeach@gmail.com>
2019-02-23 18:13:24 -0800

    updates from my copywriter

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default () => (
         I forge code, smite burritos, and like my lists in threes.
       </p>
       <p>
-        My goal is to combine pragmatic development process with my thirst to create valuable things. I care deeply about delivering quickly, iterating forward and continuous learning.
+        My goal is to combine my thirst to create valuable things with a pragmatic development process. I care deeply about delivering quickly, improving habitually, and continuous learning.
       </p>
       <p>
         I'm always looking for fun to have and stuff to build. If you want to work together, <a href="mailto:parkerjohnsonwebdev@gmail.com" className={styles.link}>shoot me an email!</a>
